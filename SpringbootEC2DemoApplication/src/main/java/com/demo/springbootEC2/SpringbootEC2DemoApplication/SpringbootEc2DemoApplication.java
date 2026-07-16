2391d332eacdf06fe9f86d01d28a0e6f58100d39
piyushsinghpbh <92782252+piyushsinghpbh@users.noreply.github.com>
2026-07-16 20:09:45 +0530

    Add files via upload

---
+++ SpringbootEC2DemoApplication/src/main/java/com/demo/springbootEC2/SpringbootEC2DemoApplication/SpringbootEc2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootEC2.SpringbootEC2DemoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEc2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEc2DemoApplication.class, args);
+	}
+
+}
---
+++ SpringbootEC2DemoApplication/src/test/java/com/demo/springbootEC2/SpringbootEC2DemoApplication/SpringbootEc2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootEC2.SpringbootEC2DemoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEc2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
