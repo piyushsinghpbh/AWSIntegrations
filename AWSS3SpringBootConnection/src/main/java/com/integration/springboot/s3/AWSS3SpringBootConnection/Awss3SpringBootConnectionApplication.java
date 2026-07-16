419dce97f4fedaa1af7774199362c2815232d825
piyushsinghpbh <92782252+piyushsinghpbh@users.noreply.github.com>
2026-07-16 20:05:47 +0530

    Add files via upload

---
+++ AWSS3SpringBootConnection/src/main/java/com/integration/springboot/s3/AWSS3SpringBootConnection/Awss3SpringBootConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.integration.springboot.s3.AWSS3SpringBootConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awss3SpringBootConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awss3SpringBootConnectionApplication.class, args);
+	}
+
+}
